14ee35b3a5b438090651713af72917ab2df3ffab
svenhopman <sven.hopman@gmail.com>
2021-10-14 15:19:57 +0200

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+function openTab(elmnt, tabName) {
+    let i, tabs, tablinks;
+    
+    // Hide all class="tab" elements by default
+    tabs = document.getElementsByClassName("tab");
+    for (i = 0; i < tabs.length; i++) {
+      tabs[i].style.display = "none";
+    }
+
+    // Set default appearance for all elements with class="tablinks"
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.borderStyle = "hidden solid hidden hidden";
+        tablinks[i].style.padding = "15px 12px 15px 15px";
+    }
+
+    // Show the current tab and update appearance for current tablink
+    document.getElementById(tabName).style.display = (tabName == 'home') ? "flex" : "block";
+    document.getElementById('closeTab').style.display = (tabName == 'home') ? "none" : "block";
+    if (elmnt.id != 'closeTab') {
+      elmnt.style.borderStyle = "solid hidden solid solid";
+      elmnt.style.padding = "12px 15px 12px 12px";
+    }
+  }
+
+openTab(null, 'home');
